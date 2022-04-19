adb01e1494bdb756d3961de27c026b909b408931
SkdRow <61521452+SkdRow@users.noreply.github.com>
2022-04-19 12:13:56 -0300

    adding meats icons

--- utils/images.js
+++ utils/images.js
@@ -3,7 +3,12 @@ export default {
     reunionIcon: require('../assets/reunion-icon.png'),
     mensIcon: require('../assets/men-icon.png'),
     womansIcon: require('../assets/woman-icon.png'),
-    ChildsIcon: require('../assets/child-icon.png')
+    ChildsIcon: require('../assets/child-icon.png'),
     //barbecueIcon: require('../assets/'),
-    //mensIcon: require('../assets/')
+    //mensIcon: require('../assets/'),
+    beefIcon: require('../assets/beef-icon.png'),
+    pigIcon: require('../assets/pig-icon.png'),
+    sheepIcon: require('../assets/sheep-icon.png'),
+    chickenIcon: require('../assets/chicken-icon.png'),
+    vegetableIcon: require('../assets/vegetable-icon.png'),
 };
