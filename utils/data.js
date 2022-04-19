1d62df9e8f4c324533cd20fa5699e73735c272b3
SkdRow <61521452+SkdRow@users.noreply.github.com>
2022-04-19 12:02:18 -0300

    adding global data

---
+++ utils/data.js
@@ -0,0 +1,37 @@
+const valores = {
+    evento: {
+        data: '',
+        nome: '',
+        local: '',
+        cityKey: ''
+    },
+    convidados: {
+        men: 0,
+        women: 0,
+        children: 0,
+        vegs: 0
+    },
+    carnes: [
+        {
+            label: '',
+            tipo: '',
+        }
+    ],
+    acompanhamentos: [
+        {
+            label: ''
+        }
+    ],
+    suprimentos: [
+        {
+            label: ''
+        }
+    ],
+    bebidas: [
+        {
+            label: ''
+        }
+    ]
+};
+
+export default valores;
