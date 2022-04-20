5bc8015893d2eb3f5bb3f2c33f87fe4c1ff65efd
SkdRow <61521452+SkdRow@users.noreply.github.com>
2022-04-20 10:22:30 -0300

    defining meats strings as array

--- utils/strings.js
+++ utils/strings.js
@@ -1,67 +1,77 @@
 export default {
-    meats: {
-        title: 'QUAIS CARNES E VEGETAIS SERÃO SERVIDOS?',
-        beef: {
-            key: 'beef',
-            description: 'CARNE BOVINA',
-            types: [
-                'Alcatra',
-                'Contra Filé',
-                'Costela Bovina',
-                'Filé Mignon',
-                'Fraldinha',
-                'Maminha',
-                'Picanha'
-            ]
+    meats: [
+        {
+            title: 'QUAIS CARNES E VEGETAIS SERÃO SERVIDOS?',
+            beef: {
+                id: 'beef',
+                description: 'CARNE BOVINA',
+                types: [
+                    'Alcatra',
+                    'Contra Filé',
+                    'Costela Bovina',
+                    'Filé Mignon',
+                    'Fraldinha',
+                    'Maminha',
+                    'Picanha'
+                ]
+            },
         },
-        pig: {
-            key: 'pig',
-            description: 'CARNE SUÍNA',
-            types: [
-                'Alcatra',
-                'Costela Suína',
-                'Linguiças frescas',
-                'Lombo',
-                'Picanha'
-            ]
+        {
+            pig: {
+                id: 'pig',
+                description: 'CARNE SUÍNA',
+                types: [
+                    'Alcatra',
+                    'Costela Suína',
+                    'Linguiças frescas',
+                    'Lombo',
+                    'Picanha'
+                ]
+            },
         },
-        sheep: {
-            key: 'sheep',
-            description: 'CARNE OVINA',
-            types: [
-                'Carré',
-                'Paleta',
-                'Pernil',
-                'Picanha de Cordeiro'
-            ]
+        {
+            sheep: {
+                id: 'sheep',
+                description: 'CARNE OVINA',
+                types: [
+                    'Carré',
+                    'Paleta',
+                    'Pernil',
+                    'Picanha de Cordeiro'
+                ]
+            },
         },
-        chicken: {
-            key: 'chicken',
-            description: 'CARNE DE FRANGO',
-            types: [
-                'Coração',
-                'Coxa',
-                'Coxa de asa',
-                'Peito',
-                'Sobrecoxa',
-                'Tulipa de frango'
-            ]
+        {
+            chicken: {
+                id: 'chicken',
+                description: 'CARNE DE FRANGO',
+                types: [
+                    'Coração',
+                    'Coxa',
+                    'Coxa de asa',
+                    'Peito',
+                    'Sobrecoxa',
+                    'Tulipa de frango'
+                ]
+            },
         },
-        vegetables: {
-            key: 'vegetables',
-            description: 'VEGETAIS',
-            types: [
-                'Abobrinha',
-                'Berinjela',
-                'Brócolis',
-                'Cebola',
-                'Cenoura',
-                'Cogumelo',
-                'Pimentão',
-                'Tomate'
-            ]
-        }
-    },
+        {
+            vegetables: {
+                id: 'vegetables',
+                description: 'VEGETAIS',
+                types: [
+                    'Abobrinha',
+                    'Berinjela',
+                    'Brócolis',
+                    'Cebola',
+                    'Cenoura',
+                    'Cogumelo',
+                    'Pimentão',
+                    'Tomate'
+                ]
+            }
+        }   
+    ],
     sideDishes: {
         types: [
             'Arroz',
